66ada7bab7dbf02a3f3993d0ca4e0d13c952c1da
hieunt0303 <bibi030301@gmail.com>
2021-09-25 10:53:08 +0700

    add comment

---
+++ script.js
@@ -0,0 +1,23 @@
+function onSuccess(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+    document.getElementById('img-gmail').src = profile.getImageUrl()
+    document.getElementById('input-namegmail').name = profile.getName()
+}
+function onFailure(error) {
+    console.log(error);
+}
+function renderButton() {
+    gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+    });
+}
